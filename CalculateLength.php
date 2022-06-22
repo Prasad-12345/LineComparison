2109c87f9846e613fa7e1ffab1cc7cd975274746
Prasad-12345 <101203154+Prasad-12345@users.noreply.github.com>
2022-06-22 16:10:14 +0530

    Program to compare the lengths of two lines

--- CalculateLength.php
+++ CalculateLength.php
@@ -5,53 +5,29 @@ class LineComparison{
     public $y1;
     public $x2;
     public $y2;
-    public $x3;
-    public $y3;
-    public $x4;
-    public $y4;
-    public $distance1;
-    public $distance2;
+    public $distance;
 
+    //function to take coordinates of line from user
     public function userInput(){
         //user input
         $this->x1 = readline("Enter x1 :");
         $this->x2 = readline("Enter x2 :");
         $this->y1 = readline("Enter y1 :");
         $this->y2 = readline("Enter y2 :");
-        $this->x3 = readline("Enter x3 :");
-        $this->x4 = readline("Enter x4 :");
-        $this->y3 = readline("Enter y3 :");
-        $this->y4 = readline("Enter y4 :");
     }
 
     //calculateLength function calculate length and returns the length
-    public function calculateLength(){
-        $this->distance1 = sqrt(pow(($this->x2 - $this->x1), 2) + pow(($this->y2 - $this->y1), 2)); 
-       // return round($distance1, 2); 
-       echo "Length of first line :" . round($this->distance1, 2) . "\n";
-        
-        $this->distance2 = sqrt(pow(($this->x4 - $this->x3), 2) + pow(($this->y4 - $this->y3), 2));
-        //return round($distance2, 2);
-        echo "Length of second line :" . round($this->distance2, 2) . "\n";
-    }  
-
-    //function to check the equality of two lines
-    // public function checkEquality(){
-    //     if($this->distance1 == $this->distance2){
-    //         echo "Two lines are equal";
-    //     }
-    //     else{
-    //         echo "Two lines are not equal";
-    //     }
-    // }
+    public function lengthOfLine(){
+       $this->distance = sqrt(pow(($this->x2 - $this->x1), 2) + pow(($this->y2 - $this->y1), 2));
+       return round($this->distance, 2);
+    }   
 
     //function to compare two lines
-    public function compareTwoLines(){
-        $check = strcasecmp($this->distance1, $this->distance2);
-        if($check > 0){
+    public function compareTwoLines($line1, $line2){
+        if($line1 > $line2){
             echo "Line1 is greater than line2";
         }
-        elseif($check < 0){
+        elseif($line1 < $line2){
             echo "Line2 is greater than line1";
         }
         else{
@@ -60,9 +36,13 @@ class LineComparison{
     }
 }   
     //Object
-    $obj = new LineComparison();
-    $obj->userInput();
-    $obj->calculateLength();
-    //$obj->checkEquality();
-    $obj->compareTwoLines();
-?>
+    $calculateLength = new LineComparison();
+    $calculateLength->userInput();
+    $line1 = $calculateLength->lengthOfLine();
+    echo "Length of line1 is :" . $line1 . "\n";
+
+    $calculateLength->userInput();
+    $line2 = $calculateLength->lengthOfLine();
+    echo "Length of line2 is :" . $line2 . "\n";
+
+    $calculateLength->compareTwoLines($line1, $line2);
